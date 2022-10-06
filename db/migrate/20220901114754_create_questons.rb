c2c121a256ac90cd9b478a64bb0e66138eb20850
Foma <fominlzpv@gmail.com>
2022-10-06 15:51:06 +0300

    lesson-3(4)

--- db/migrate/20220901114754_create_questons.rb
+++ db/migrate/20220901114754_create_questons.rb
@@ -2,7 +2,7 @@ class CreateQuestons < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
       t.string :title, null: false
-      t.boolean :correct, null: false, default: true
+      t.boolean :correct, null: false, default: false
       t.references :test, null: false, foreign_key: true
 
       t.timestamps
