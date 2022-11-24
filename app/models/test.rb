7dc7ffd86b3bcbe445f481571ec6a1a4621c2401
Foma <fominlzpv@gmail.com>
2022-11-24 21:26:43 +0300

    lesson-4(4)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,11 @@
 class Test < ApplicationRecord
-  has_many :users
+  has_many :tests_users
+  has_many :users, through: :tests_users
 
   def self.list_test_by_category(category)
-    Test.joins("JOIN categories ON tests.category_id = categories.id")
-        .where(categories: {title: category} )
-        .order("categories.title DESC")
-        .pluck("categories.title")
+    joins("JOIN categories ON tests.category_id = categories.id")
+      .where(categories: {title: category} )
+      .order("categories.title DESC")
+      .pluck("tests.title")
   end  
 end
