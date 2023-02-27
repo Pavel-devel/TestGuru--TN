24d230da320db22f25d0ee7a0d4442ea033a8ff9
Foma <fominlzpv@gmail.com>
2023-02-27 21:00:06 +0300

    adding and editing migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,66 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_160247) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_174524) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
-    t.boolean "correct", default: false, null: false
-    t.bigint "question_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
+  create_table 'answers', force: :cascade do |t|
+    t.string 'body', null: false
+    t.boolean 'correct', default: false, null: false
+    t.bigint 'question_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'title', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "body", null: false
-    t.bigint "test_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_questions_on_test_id"
+  create_table 'questions', force: :cascade do |t|
+    t.string 'body', null: false
+    t.bigint 'test_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0
-    t.bigint "category_id", null: false
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_tests_on_author_id"
-    t.index ["category_id"], name: "index_tests_on_category_id"
+  create_table 'tests', force: :cascade do |t|
+    t.string 'title', null: false
+    t.integer 'level', default: 0
+    t.bigint 'category_id', null: false
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_id'], name: 'index_tests_on_author_id'
+    t.index ['category_id'], name: 'index_tests_on_category_id'
+    t.index %w[title level], name: 'index_tests_on_title_and_level', unique: true
   end
 
-  create_table "tests_users", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "test_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["test_id"], name: "index_tests_users_on_test_id"
-    t.index ["user_id"], name: "index_tests_users_on_user_id"
+  create_table 'tests_users', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'test_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['test_id'], name: 'index_tests_users_on_test_id'
+    t.index ['user_id'], name: 'index_tests_users_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
-    t.string "password", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', null: false
+    t.string 'password', null: false
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "categories"
-  add_foreign_key "tests", "users", column: "author_id"
-  add_foreign_key "tests_users", "tests"
-  add_foreign_key "tests_users", "users"
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'questions', 'tests'
+  add_foreign_key 'tests', 'categories'
+  add_foreign_key 'tests', 'users', column: 'author_id'
+  add_foreign_key 'tests_users', 'tests'
+  add_foreign_key 'tests_users', 'users'
 end
