83316e76473abb40e6b4875d7ee8d37af61c6896
Foma <fominlzpv@gmail.com>
2023-03-29 00:48:34 +0300

    controllers__and_views_v2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,20 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[new create index show]
   before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
-  def index
-    @questions = Question.all
-  end
+  def index; end
 
   def show; end
 
   def new
-    @question = Question.new
+    @question = Question.new(test_id: @test.id)
   end
 
   def create
-    @question = Question.new(question_params)
+    @question = @test.questions.build(question_params)
     if @question.save
-      redirect_to @question
+      redirect_to test_questions_path(@test)
     else
       render :new
     end
@@ -29,11 +28,15 @@ class QuestionsController < ApplicationController
   private
 
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 
   def rescue_with_test_not_found
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,13 +1,11 @@
 class TestsController < ApplicationController
   def index
     @tests = Test.all
-
-    render plain: @tests
   end
 
   def show
     @test = Test.find(params[:id])
 
-    render plain: @test
+    render json: @test.questions
   end
 end
